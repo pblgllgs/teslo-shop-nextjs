9f18636b275ba8542cc72afd2a03822b6e3c4004
pblgllgs <pbl.gllgs@gmail.com>
2022-06-07 12:45:11 -0400

    clear cookies address

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -98,6 +98,14 @@ export const AuthProvider: FC<Props> = ({ children }) => {
   const logoutUser = () => {
     Cookies.remove('token');
     Cookies.remove('cart');
+    Cookies.remove('firstName');
+    Cookies.remove('lastName');
+    Cookies.remove('address');
+    Cookies.remove('address2');
+    Cookies.remove('zip');
+    Cookies.remove('city');
+    Cookies.remove('country');
+    Cookies.remove('phone');
     router.reload();
   };
 
