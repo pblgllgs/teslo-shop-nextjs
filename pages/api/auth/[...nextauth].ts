2223d907ccc53405590ab5c7faafc59f52894b1d
pblgllgs <pbl.gllgs@gmail.com>
2022-06-08 11:22:38 -0400

    dos puntos

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials"
-import { dbUsers } from "../../../database"
+import { dbUsers } from "../../../database";
 
 export default NextAuth({
     providers: [
